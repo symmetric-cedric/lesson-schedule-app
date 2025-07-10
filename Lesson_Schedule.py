9489c7ed22721b5ea83a1c9c6631fa66cd8486c4
symmetric-cedric <cedricpoon310@gmail.com>
2025-07-10 22:53:39 +0100

    Update Lesson_Schedule.py

--- Lesson_Schedule.py
+++ Lesson_Schedule.py
@@ -158,7 +158,23 @@ def fill_template_doc(
     template_path
 ):
     doc = Document(template_path)
+    
+    bank_info_map = {
+    "九龍灣(淘大)分校": "恆生銀行\nYCH EDUCATION LIMITED\n369 439 963 883",
+    "藍田(麗港城)分校": "恆生銀行\nYCL EDUCATION LIMITED\n244 257 796 883",
+    "青衣(青怡)分校": "匯豐銀行\nLCH EDUCATION LIMITED\n049 656 150 838",
+    "九龍站(港景峯)分校": "恆生銀行\nMISS MAN EDUCATION LIMITED\n244 333 787 883",
+    "鑽石山(萬迪廣場)分校": "恆生銀行\nMISS MAN EDUCATION LIMITED\n244 333 787 883"
+    }
 
+    quick_transfer_map = {
+        "淘大": "YCH EDUCATION LIMITED\nFPS: 1611 070 16",
+        "麗港城": "恆生銀行\nYCL EDUCATION LIMITED\n244 257 796 883",
+        "青衣(青怡)分校": "LCH EDUCATION LIMITED\nFPS: 1608 3 2457",
+        "港景峯": "MISS MAN EDUCATION LIMITED\nFPS: 103 779 914",
+        "鑽石山": "MISS MAN EDUCATION LIMITED\nFPS: 103 779 914"
+    }
+    
     # Replace placeholders
     start_date_str = start_date.strftime('%d/%m/%Y')
     end_date = start_date + timedelta(weeks=week_range) - timedelta(days=1)
@@ -174,6 +190,8 @@ def fill_template_doc(
         "上課期數範圍": f"上課期數範圍：{date_range_str}",
         "分校": branch_name,
         "上課時間：": f"上課時間：{day_time_str}"
+        "Bank_info": bank_info_map.get(branch_name),
+        "Quick_transfer": quick_transfer_map.get(branch_name)
     }
 
     for para in doc.paragraphs:
