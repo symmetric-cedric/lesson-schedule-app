9608c3bdc6fdebf65262f134d9d41bae924afdbe
symmetric-cedric <cedricpoon310@gmail.com>
2025-07-10 22:20:40 +0100

    Update Lesson_Schedule.py

--- Lesson_Schedule.py
+++ Lesson_Schedule.py
@@ -170,7 +170,7 @@ def fill_template_doc(
         "堂數：": f"堂數：{total_lessons}",
         "上課期數範圍": f"上課期數範圍：{date_range_str}",
         "分校": branch_name,
-        "上課時間": f"上課時間：{day} {time}" for day, time in day_time_pairs.items()
+        "上課時間：": f"上課時間：{day_time_str}"
     }
 
     for para in doc.paragraphs:
