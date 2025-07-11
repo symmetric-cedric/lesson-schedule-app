74b86ee7faaea3ff4c50cfae9390b0f15fd57a96
symmetric-cedric <cedricpoon310@gmail.com>
2025-07-11 17:43:24 +0100

    Update Lesson_Schedule.py

--- Lesson_Schedule.py
+++ Lesson_Schedule.py
@@ -312,7 +312,7 @@ preview_lesson_dates, _ = generate_schedule(
 show_cancel = st.checkbox("是否有取消上課日期？", value=False)
 st.markdown("---")
 st.markdown("<hr style='border: 1px solid #ccc;'>", unsafe_allow_html=True)
-st.markdown("### 🧾 收據資訊")
+st.markdown("### 🧾 學費資訊")
 
 cancel_holidays = []
 
